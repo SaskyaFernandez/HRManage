a2e256b76d56aff0ed6f88af60d82e6247a9993b
saskya <fernandezsaskya@gmail.com>
2024-04-21 21:13:32 +0200

    create user controller

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const userController = require('./controllers/UserController');
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT;
+
+app.use('/api/users', userController);
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
