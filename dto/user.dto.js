7e4d286ceb48ef721dab78be2f6d93d92ee55fca
Saskya <fernandezsaskya@gmail.com>
2024-04-24 19:21:09 +0200

    add user dto

---
+++ dto/user.dto.js
@@ -0,0 +1,32 @@
+/**
+ * User
+ * @typedef {object} userDTO
+ * @property {number} id
+ * @property {string} firstname
+ * @property {string} lastname
+ * @property {string} email
+ * @property {string} password
+ * @property {roleDTO[]} role
+ * @property {string} entryDate
+ * @property {boolean} isDeleted
+ * @property {number} maxHolidays
+ * @property {number} holidaysLeft
+ * @property {string} image
+*/
+export class UserDTO {
+
+    constructor({ id, firstname, lastname, email, password, role, entryDate, isDeleted, maxHolidays, holidaysLeft, image }) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.entryDate = entryDate;
+        this.isDeleted = isDeleted;
+        this.maxHolidays = maxHolidays;
+        this.holidaysLeft = holidaysLeft;
+        this.image = image;
+
+    }
+}
