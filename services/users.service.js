aa618388a2d761ff9e5839043a411de12b3be723
Saskya <fernandezsaskya@gmail.com>
2024-04-24 19:19:40 +0200

    add user services

---
+++ services/users.service.js
@@ -0,0 +1,17 @@
+import { UserDTO } from '../dto/user.dto.js';
+import db from "../models/index.models.js";
+
+const usersServices = {
+    add: async (userData) => {
+        const user = await db.Users.create(userData);
+        return new UserDTO(user);
+    },
+    getAll: async () =>{
+        const users = await db.Users.findAll();
+        return users.map(user => user.firstname)
+    },
+    getUserById: async () => {
+        
+    }
+}
+export default usersServices;
