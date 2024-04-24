6e7f0c94c03da49e59886f96907ba6b55bf65a43
Saskya <fernandezsaskya@gmail.com>
2024-04-24 19:21:39 +0200

    add role dto

---
+++ dto/role.dto .js
@@ -0,0 +1,13 @@
+/**
+ * User
+ * @typedef {object} roleDTO
+ * @property {number} id
+ * @property {string} name
+*/
+export class roleDTO {
+
+    constructor({ id, name}) {
+        this.id = id;
+        this.firstname = name;
+    }
+}
