ab7d39da8a7b886c83ba801f0626d8bfbd28fa96
saskyafernandez <fernandezsaskya@gmail.com>
2024-05-01 21:13:16 +0200

    cors origin

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ import userRouter from './routes/users.router.js';
 import expressJSDocSwagger from 'express-jsdoc-swagger';
 import swaggerOption from './swagger.option.js';
 import authRouter from './routes/Auth.router.js';
-import { middlewareAuthentification } from './middleware/index.middleware.js';
+import { middlewareAuthentification, middlewareAuthorisation } from './middleware/index.middleware.js';
 import holidaysRouter from './routes/holidays.router.js';
 import rolesRouter from './routes/roles.router.js';
+import cors from 'cors'; 
 
 const app = express();
 const port = process.env.PORT || 3000;
 expressJSDocSwagger(app)(swaggerOption);
 
+const corsOption = {
+    origin: 'http://localhost:5173'
+}
+app.use(cors(corsOption));
+
 app.use(express.json()) 
 app.use('/api/auth', authRouter);
 app.use(middlewareAuthentification);
+app.use(middlewareAuthorisation);
 app.use('/api/users', userRouter);
 app.use('/api/holidays', holidaysRouter);
 app.use('/api/roles', rolesRouter);
