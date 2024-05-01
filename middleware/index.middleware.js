bbd962ea0948206c04ef5d06c9df43b74a71e7d5
saskyafernandez <fernandezsaskya@gmail.com>
2024-05-01 21:13:00 +0200

    adding middleware authorisation

--- middleware/index.middleware.js
+++ middleware/index.middleware.js
@@ -20,3 +20,28 @@ const middlewareAuthentification = async (req, res, next) => {
 };
 
 export { middlewareAuthentification };
+
+
+const middlewareAuthorisation = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) {
+        return res.status(403).json({ error: "Access denied." });
+    }
+
+    try {
+        let payload = jwtTokenDecrypted(token);
+        let userRoles = payload.userRole.includes('RH')
+        if (!userRoles) {
+            return res.status(403).json({ error: "You don't have the necessary rights" });
+
+            }
+            next();
+    } catch (error) {
+
+        return res.status(403).json({ error: "Token invalid." });
+    }
+};
+
+export { middlewareAuthorisation };
